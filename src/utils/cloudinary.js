e707113d3c5c3b5af8532c87e08c0bf8a3135904
Vivek <vivsharma571@gmail.com>
2024-09-18 12:25:06 +0530

    Used and configured multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+
+// fs = filesystem this library comes by default with Node.js so it can be used anywhere
+// we use the filesystem to manage the entire file system eg open,read,permission etc....
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(
+            localFilePath,
+            // These are upload options
+            {
+                resource_type: "auto",
+            }
+        );
+        // file has been uploaded successfully
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        // fs.unlinkSync() is a method in Node.js used to synchronously delete a file from the file system. It is part of the fs (File System) module, which provides functions for interacting with the file system
+        // In Node.js, both fs.unlink() and fs.unlinkSync() are used to delete files from the file system. The key difference between them lies in their execution:
+        // fs.unlink(): Asynchronous (non-blocking).
+        // fs.unlinkSync(): Synchronous (blocking).
+        fs.unlinkSync(localFilePath)  //remove the locally saved temporary file as the upload operation gets failed
+    }
+};
+
+
+export { uploadOnCloudinary }
